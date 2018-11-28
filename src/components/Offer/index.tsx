0010d94dd758c68f4881c9e58aef7a1972556a3c
dominik-zeglen <flesz3@o2.pl>
2018-11-28 22:51:19 +0100

    Enable adding offers with no content

--- src/components/Offer/index.tsx
+++ src/components/Offer/index.tsx
@@ -1,35 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
-import {getPageFields, IPage} from '../../utils';
-import RichText from '../RichText'
-import './style.scss';
+import { getPageFields, IPage } from "../../utils";
+import RichText from "../RichText";
+import "./style.scss";
 
 interface IOfferProps {
   offer: IPage;
   reverse: boolean;
 }
 
-export const Offer: React.SFC<IOfferProps> = ({offer, reverse}) => {
+export const Offer: React.SFC<IOfferProps> = ({ offer, reverse }) => {
   const fields = getPageFields(offer);
-  return reverse ? (
+  return fields.content === undefined ? (
+    <>
+      <div className="offer__header-container">
+        <h3 className="offer__header">{offer.name}</h3>
+      </div>
+      <div className="offer">
+        <div className="offer__image-container offer__image-container--full">
+          <img className="offer__image" src={"/static/" + fields.image} />
+        </div>
+      </div>
+    </>
+  ) : reverse ? (
     <div className="offer">
       <div className="offer__content-container">
-        <h3 className="offer__content-header">{offer.name}</h3>
+        <h3 className="offer__header">{offer.name}</h3>
         <div className="offer__content">
           <RichText content={fields.content} />
         </div>
       </div>
       <div className="offer__image-container">
-        <img className="offer__image" src={'/static/' + fields.image} />
+        <img className="offer__image" src={"/static/" + fields.image} />
       </div>
     </div>
   ) : (
     <div className="offer">
       <div className="offer__image-container">
-        <img className="offer__image" src={'/static/' + fields.image} />
+        <img className="offer__image" src={"/static/" + fields.image} />
       </div>
       <div className="offer__content-container">
-        <h3 className="offer__content-header">{offer.name}</h3>
+        <h3 className="offer__header">{offer.name}</h3>
         <div className="offer__content">
           <RichText content={fields.content} />
         </div>
